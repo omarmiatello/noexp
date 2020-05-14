e5f4d76a909974c5ad0ed0c0c61865bd3435a32a
Omar Miatello <jackl85@gmail.com>
2020-05-15 01:07:58 +0200

    add `categories-parser` module

---
+++ categories-parser/src/main/kotlin/CategoriesParser.kt
@@ -0,0 +1,70 @@
+package com.github.omarmiatello.noexp
+
+import java.io.File
+
+fun parseNoExpCategories(file: File): List<Category> {
+    var currentParents = listOf("")
+    var tab = 0
+    val childParents = file.readLines().drop(1).map { it.toCategoryLine() }.map { line ->
+        val parents = when (val diff = line.indent - tab) {
+            in -10..0 -> currentParents.dropLast(1 - diff)
+            1 -> currentParents
+            else -> error("The number of tabs should beetween -10/+1: $line $diff")
+        }
+        currentParents = parents + line.name
+        tab = line.indent
+        line to parents
+    }
+    val children = childParents.groupBy({ it.second.lastOrNull() }, { it.first.name })
+    val categories = childParents.map { (line, parents) ->
+        Category(
+            name = line.name,
+            alias = line.alias,
+            directParent = parents.lastOrNull(),
+            allParents = parents,
+            directChildren = children[line.name].orEmpty(),
+            min = line.min,
+            desired = line.desired,
+            max = line.max,
+            maxPerWeek = line.maxPerWeek,
+            maxPerYear = line.maxPerYear
+        )
+    }.sorted()
+    val categoryDuplicated = categories.flatMap { it.alias + it.name }.groupBy { it }.filterValues { it.size > 1 }.keys
+    if (categoryDuplicated.isNotEmpty()) error("There are some categories duplicated: ${categoryDuplicated.joinToString()}")
+    file.writeText(categories.toFormattedString())
+    return categories
+}
+
+private fun String.toCategoryLine(): CategoryLine {
+    val tabsCount = takeWhile { !it.isLetter() }.let { nonLetter ->
+        val space = nonLetter.count { it == ' ' }
+        nonLetter.count() - space + (space / 4)
+    }
+    if (tabsCount > 10) error("Are you sure? Seems too depth ($tabsCount tabs)")
+    val (name, quantity, week, year, alias) = split("|").map { it.trim() }
+    val (min, desired, max) = quantity.split(" ").map { it.toInt().takeIf { it != 0 } }
+    return CategoryLine(
+        indent = tabsCount,
+        name = name,
+        alias = alias.split(",").map { it.trim() } - "",
+        min = min,
+        desired = desired,
+        max = max,
+        maxPerWeek = week.toIntOrNull(),
+        maxPerYear = year.toIntOrNull()
+    )
+}
+
+private fun List<Category>.toFormattedString() = "${"--==[ Categories ]==--".padEnd(40)} | quantity | week | year | alias\n" + joinToString("\n") {
+    buildString {
+        append(it.allParents.joinToString("") { "\t" })
+        append(it.name.padEnd(40 - (it.allParents.size * 4)))
+        append(" | ${it.min ?: 0} ${it.desired ?: 0} ${it.max ?: 0}".padEnd(11))
+        append(" | ${if (it.maxPerWeek ?: 0 > 0) it.maxPerWeek else ""}".padEnd(7))
+        append(" | ${if (it.maxPerYear ?: 0 > 0) it.maxPerYear else ""}".padEnd(7))
+        append(" | ${it.alias.joinToString()}")
+    }
+}
+
+private class CategoryLine(val indent: Int, val name: String, val alias: List<String>, val min: Int?, val desired: Int?, val max: Int?, val maxPerWeek: Int?, val maxPerYear: Int?)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "noexp"
 include("dataclass")
+include("categories-parser")
