323a15cedd60b56419030986fa88f7b66c687032
Omar Miatello <jackl85@gmail.com>
2021-02-20 22:00:30 +0100

    Version 0.5.0 - Add DateUtils.formatRelative()

--- dataclass/src/main/kotlin/utils/DateUtils.kt
+++ dataclass/src/main/kotlin/utils/DateUtils.kt
@@ -10,12 +10,15 @@ object DateUtils {
     fun millisToDays(dateMillis: Long, now: Long = System.currentTimeMillis()) =
         (dateMillis - now) / (1000 * 60 * 60 * 24)
 
-    fun formatRelativeShort(dateMillis: Long, now: Long = System.currentTimeMillis()): String {
-        val h = ((dateMillis - now) / (1000 * 60 * 60)).also { if (abs(it) < 36) return "${it}h" }
-        val d = (h / 24).also { if (abs(it) < 45) return "${it}d" }
-        val m = (d / 30).also { if (abs(it) < 13) return "${it}M" }
-        return "${m / 12}y"
+    fun formatRelative(dateMillis: Long, now: Long = System.currentTimeMillis()): Pair<Int, String> {
+        val h = ((dateMillis - now) / (1000 * 60 * 60)).toInt().also { if (abs(it) < 36) return it to "h" }
+        val d = (h / 24).also { if (abs(it) < 45) return it to "d" }
+        val m = (d / 30).also { if (abs(it) < 13) return it to "M" }
+        return m / 12 to "y"
     }
+
+    fun formatRelativeShort(dateMillis: Long, now: Long = System.currentTimeMillis()) =
+        with(formatRelative(dateMillis, now)) { "$first$second" }
 
     fun formatDateItalian(dateMillis: Long): String = dateFormat.format(Date(dateMillis))
 }
